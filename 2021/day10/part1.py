8d24a1d1c71d8768e80641b32bb33a266631a741
kevinserrao <kevin@neighborschools.com>
2021-12-17 21:19:52 -0500

    temp

--- 2021/day10/part1.py
+++ 2021/day10/part1.py
@@ -5,23 +5,34 @@ def parseInput():
         f.close()
         return lines
 
-def isLowPoint(grid, x, y):
-    for [i,j] in [[0,1], [1,0], [-1,0], [0,-1]]:
-        if x + i > -1 and x + i < len(grid) and y + j > -1 and y + j < len(grid[x+i]):
-            if grid[x][y] >= grid[x+i][y+j]:
-                return False
-    return True
+scoreForCorruptedCharacter = {')': 3, ']': 57, '}': 1197, '>': 25137, '*': 0}
 
+def getCorruptedCharacter(line):
+    stack = []
+    for c in line:
+        if c in ['(','{','[', '<']:
+            stack.push(c)
+        else:
+            l = stack.pop()
+            if c == ')':
+                if l != '(':
+                    return c
+            elif c == ']':
+                if l != '[':
+                    return c
+            elif c == '}':
+                if l != '{':
+                    return c
+            elif c == '>':
+                if l != '<':
+                    return c
+    return '*'
 
 def main():
     lines = parseInput()
-    grid = [[int(c) for c in line] for line in lines]
-    sumRiskLevel = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if isLowPoint(grid, i, j):
-                print(i,j)
-                sumRiskLevel += grid[i][j] + 1
-    print(sumRiskLevel)
+    totalScore = 0
+    for line in lines:
+        totalScore += scoreForCorruptedCharacter[getCorruptedCharacter(line)]
+    print(totalScore)
 
 main()
